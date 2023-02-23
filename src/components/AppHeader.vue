4963d9316f3eaf66f686033c498217da9c933e20
Thai Doan Son <thai.doan.son@azoom.jp>
2023-02-23 10:19:47 +0700

    👍 Implement header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-header">
+    <h1>My Tello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped>
+.app-header {
+  background-color: #d3a1eb;
+  text-align: center;
+}
+</style>
